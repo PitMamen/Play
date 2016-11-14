3794498bae51498cf80dbba05ad224a76716d44f
PXK <541931879@qq.com>
2016-11-14 11:59:29 +0800

    add auto connect bluetooth devices

---
+++ myBuleTooth/src/main/java/com/example/mybuletooth/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.mybuletooth;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+	
+	/** Called when the activity is first created. */ 
+	private Button autopairbtn=null;
+	private BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		autopairbtn=(Button) findViewById(R.id.button1);
+		autopairbtn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (!bluetoothAdapter.isEnabled())
+				{
+					bluetoothAdapter.enable();//异步的，不会等待结果，直接返回。
+				}else{
+					bluetoothAdapter.startDiscovery();
+				}
+			}
+		});
+		
+	}
+	
+}
